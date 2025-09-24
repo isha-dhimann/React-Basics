81eb48126a47e1619f0eb99835c76e3ebc675e69
Isha-dhimann <ishadhiman2681@gmail.com>
2025-09-24 10:36:39 +0530

    24/9/25

---
+++ css_demo/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+// import './App.css'
+
+   // App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ color: "blue", textAlign: "center" }}>
+        Welcome to My React App !
+      </h1>
+
+      <p style={{ color: "green", fontSize: "30px", backgroundColor: "#999",
+        fontStyle:"italic", padding: "10px" }}>
+      My Name is Ishaa..........
+      </p>
+
+      <p style={{ fontStyle: "bold", color: "purple" , fontSize:"1.6rem" }}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae provident labore animi eaque praesentium maxime laudantium vitae magni, tenetur quo quia reiciendis. Exercitationem et dolore sit aspernatur labore commodi.
+      Fuga soluta in aperiam incidunt doloremque fugiat. Nulla architecto, natus labore perspiciatis excepturi ipsam expedita sequi amet voluptatum voluptas, ratione magni vitae pariatur quibusdam! Dicta ipsa quia perspiciatis fuga sunt.
+      Similique corrupti beatae corporis dicta ipsum ab voluptates tempora ipsam perferendis maiores quis mollitia, aut sunt itaque ex nihil porro quo exercitationem iste aperiam esse quae veritatis libero voluptas. Perferendis.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae provident labore animi eaque praesentium maxime laudantium vitae magni, tenetur quo quia reiciendis. Exercitationem et dolore sit aspernatur labore commodi.
+      Fuga soluta in aperiam incidunt doloremque fugiat. Nulla architecto, natus labore perspiciatis excepturi ipsam expedita sequi amet voluptatum voluptas, ratione magni vitae pariatur quibusdam! Dicta ipsa quia perspiciatis fuga sunt.
+      Similique corrupti beatae corporis dicta ipsum ab voluptates tempora ipsam perferendis maiores qu Fuga soluta in aperiam incidunt doloremque fugiat. Nulla architecto, natus labore perspiciatis excepturi ipsam expedita sequi amet voluptatum voluptas, ratione magni vitae pariatur quibusdam! Dicta ipsa quia perspiciatis fuga sunt.
+      Similique corrupti beatae corporis dicta ipsum ab voluptates tempora ipsam perferendis maiores qu</p>
+
+      <button style={{ backgroundColor: "orange", color: "white", border: "none", borderRadius: "5px"
+        , margin: "20px 90px"
+       }}>
+        Click Me
+      </button>
+<br></br>
+      <input style={{margin:"20px 40px" , fontSize:"30px" , backgroundColor:"white" }} type = "text" placeholder='searching here...'/>
+    </>
+  );
+}
+
+export default App;
+
--- props_concept/src/component/wish.jsx
+++
@@ -1,7 +0,0 @@
-
-
-function Wish(props) {
-    return <h1>{props.msg}{props.name}</h1>
-}
-
-export default Wish;
